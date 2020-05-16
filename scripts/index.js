af7d0960b7c6344faff81eaf637342e3a2d6a564
Anton Afanasiev <anton.afanasyev93@yandex.ru>
2020-05-16 23:47:21 +0300

    правки по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
 const container = document.querySelector('.root');
 const popup = container.querySelector('.popup');
 const editButton = container.querySelector('.profile__editbutton');
-const popupName = container.querySelector('.popup__name');
-const popupDescr = container.querySelector('.popup__descr');
+const popupName = container.querySelector('.popup__input_name');
+const popupDescr = container.querySelector('.popup__input_descr');
 const nameProfile = container.querySelector('.profile__name');
 const descrProfile = container.querySelector('.profile__descr');
 const closeButton = container.querySelector('.popup__close-button');
-const saveButton = container.querySelector('.popup__savebutton');
 
 function openForm() {
   popup.classList.add('popup_opened');
@@ -19,7 +18,7 @@ editButton.addEventListener('click', openForm);
 
 function closeForm() {
   popup.classList.toggle('popup_opened');
-  container.classList.toggle('root_scroll-hidden');
+  // container.classList.toggle('root_scroll-hidden');
 }
 
 closeButton.addEventListener('click', closeForm);
@@ -28,15 +27,9 @@ let formElement = container.querySelector('.popup__form');
 
   function formSubmitHandler (evt) {
     evt.preventDefault();
-    let nameInput = container.querySelector('.popup__name');
-    let jobInput = container.querySelector('.popup__descr');
-    let name = nameInput.value;
-    let job = jobInput.value;
-    let profileName = container.querySelector('.profile__name');
-    let profileDescr = container.querySelector('.profile__descr');
-    profileName.textContent = name;
-    profileDescr.textContent = job;
+    nameProfile.textContent = popupName.value;
+    descrProfile.textContent = popupDescr.value;
+    closeForm()
   }
 
 formElement.addEventListener('submit', formSubmitHandler);
-saveButton.addEventListener('click', closeForm);
