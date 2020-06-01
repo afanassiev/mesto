ef1c952616a479ad8f4658e28b7d847c7a543628
Anton Afanasiev <anton.afanasyev93@yandex.ru>
2020-06-01 18:32:45 +0300

    правки по первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -16,72 +16,75 @@ const addButton = document.querySelector('.profile__addbutton');
 const formAdder = container.querySelector('.popup__add');
 const formElement = container.querySelector('.popup__form_profile');
 const formElementSecond = container.querySelector('.popup__form_cards');
+const newItemName = document.querySelector('.popup__input_placename');
+const newItemLink = document.querySelector('.popup__input_link');
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   },
   {
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   }
 ];
-let newItemName = document.querySelector('.popup__input_placename');
-let newItemLink = document.querySelector('.popup__input_link');
 
 function getInput() {
   popupName.value = nameProfile.textContent;
   popupDescr.value = descrProfile.textContent;
 }
 
+function popupToggle() {
+  (popup.classList.toggle('popup_opened'));
+}
+
 function openForm() {
-  popup.classList.toggle('popup_opened');
+  popupToggle();
   getInput();
 }
-editButton.addEventListener('click', openForm);
 
-function closeForm() {
-  popup.classList.toggle('popup_opened');
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = popupName.value;
+  descrProfile.textContent = popupDescr.value;
+  popupToggle();
 }
-closeButton.addEventListener('click', closeForm);
 
-  function formSubmitHandler (evt) {
-    evt.preventDefault();
-    nameProfile.textContent = popupName.value;
-    descrProfile.textContent = popupDescr.value;
-    closeForm();
-  }
-formElement.addEventListener('submit', formSubmitHandler);
+function addFormToggle() {
+  formAdder.classList.toggle('popup_opened');
+}
+
+function cardSubmit (evt) {
+  evt.preventDefault();
+  addCard(newItemName.value, newItemLink.value);
+  addFormToggle();
+}
 
 function closeButtons() {
   let closeButtonArray = document.querySelectorAll('.popup__close-button');
-  console.log(closeButtonArray);
-  closeButtonArray.forEach(item => item.addEventListener('click', closeButtonEvent));
+  closeButtonArray.forEach(item => item.addEventListener('click', function (event) {
+    let eventTarget = event.target.parentElement.parentElement;
+    eventTarget.classList.remove('popup_opened');
+  }));
 }
 
-function closeButtonEvent(event) {
-  let eventTarget = event.target.parentElement.parentElement;
-  console.log(eventTarget);
-  eventTarget.classList.remove('popup_opened');
-}
-
-function getCard (name, link) {
+function addCard (name, link) {
   let itemElement = itemTemplate.cloneNode(true);
   let itemImage = itemElement.querySelector('.item__image');
   itemImage.src = link;
@@ -98,51 +101,18 @@ function getCard (name, link) {
   itemElement.querySelector('.item__delete-button').addEventListener('click', function (evt) {
     evt.target.parentElement.remove();
   });
-  itemContainer.append(itemElement);
-}
-
-initialCards.forEach(function (itemElement) {
-  getCard(itemElement.name, itemElement.link);
-});
-
-addButton.addEventListener('click', openAdder);
-
-function openAdder() {
-  formAdder.classList.toggle('popup_opened');
-}
-
-function addCard () {
-  let itemElement = itemTemplate.cloneNode(true);
-  let itemImage = itemElement.querySelector('.item__image');
-  itemImage.src = newItemLink.value;
-  // itemElement.querySelector('.item__image').src = newItemLink.value;
-  itemElement.querySelector('.item__title').textContent = newItemName.value;
-  itemImage.addEventListener('click', function (evt) {
-    imagePopup.classList.toggle('popup_opened');
-    imagePic.src = newItemLink.value;
-    imageName.textContent = newItemName.value;
-    closeButtons();
-  });
-  itemElement.querySelector('.item__checkbox').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('item__checkbox_active');
-  });
-  itemElement.querySelector('.item__delete-button').addEventListener('click', function (evt) {
-    evt.target.parentElement.remove();
-  });
   itemContainer.prepend(itemElement);
 }
 
-function cardSubmit (evt) {
-  evt.preventDefault();
-  addCard();
-  closeFormCards();
-}
+initialCards.forEach(function (itemElement) {
+  addCard(itemElement.name, itemElement.link);
+});
 
+editButton.addEventListener('click', openForm);
+addButton.addEventListener('click', addFormToggle);
+
+closeButton.addEventListener('click', closeButtons);
+closeButtonCards.addEventListener('click', closeButtons);
+
+formElement.addEventListener('submit', formSubmitHandler);
 formElementSecond.addEventListener('submit', cardSubmit);
-
-function closeFormCards() {
-  formAdder.classList.toggle('popup_opened');
-}
-
-closeButtonCards.addEventListener('click', closeFormCards);
-
